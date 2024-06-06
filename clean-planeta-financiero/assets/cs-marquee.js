44c74e12fc53e549ae237744c489a73f31d47fed
eduardojaoel <eduardojaoel@gmail.com>
2024-06-05 19:02:12 -0500

    Ajustes a la página de inicio

--- clean-planeta-financiero/assets/cs-marquee.js
+++ clean-planeta-financiero/assets/cs-marquee.js
@@ -1,19 +1,19 @@
 function findSliders(eventId) {
-  document.querySelectorAll('.cs-slider.splide').forEach((slider) => {
+  document.querySelectorAll('.cs-marquee.splide').forEach((slider) => {
     /* console.log('eventId:', eventId);
     console.log('slider.id:', slider.getAttribute('section-id')); */
-    new Splide(`#${slider.id}`).mount();
+    new Splide(`#${slider.id}`).mount(window.splide.Extensions);
   });
 }
 
 document.addEventListener('DOMContentLoaded', findSliders);
 
 document.addEventListener('shopify:section:load', (event) => {
-  document.querySelectorAll('.cs-slider.splide').forEach((slider) => {
+  document.querySelectorAll('.cs-marquee.splide').forEach((slider) => {
     /* console.log(event.target.id);
     console.log(slider.getAttribute('section-id')); */
     if (event.target.id == slider.getAttribute('section-id')) {
-      new Splide(`#${slider.id}`).mount();
+      new Splide(`#${slider.id}`).mount(window.splide.Extensions);
     }
   });
 });
